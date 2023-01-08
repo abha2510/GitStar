c10bd1cfcaf042139cf6979502a10e61d71908ff
Abha Meshram <abha25meshram@gmail.com>
2023-01-08 21:32:57 +0530

    change

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    unoptimized:true,
+  }
 }
 
-module.exports = {
-images: {
-  domains: ['avatars.githubusercontent.com'],
-},
-}
+module.exports = nextConfig;
